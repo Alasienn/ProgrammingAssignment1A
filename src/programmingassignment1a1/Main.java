5b669ddbffdf3dda83095908270804923b3c2181
Alassien <Alassien@Mancera-Alfa>
2018-09-10 23:21:04 -0500

    lectura de archivo

--- src/programmingassignment1a1/Main.java
+++ src/programmingassignment1a1/Main.java
@@ -22,6 +22,25 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws FileNotFoundException {
+        try {
+            // Abrimos el archivo
+            FileInputStream fstream = new FileInputStream("entrada.txt");
+            // Creamos el objeto de entrada
+            DataInputStream entrada = new DataInputStream(fstream);
+            // Creamos el Buffer de Lectura
+            BufferedReader buffer = new BufferedReader(new InputStreamReader(entrada));
+            String strLinea;
+            // Leer el archivo linea por linea
+            while ((strLinea = buffer.readLine()) != null) {
+                // Imprimimos la línea por pantalla
+                System.out.println(strLinea);
+            }
+            // Cerramos el archivo
+            entrada.close();
+        } catch (Exception e) { //Catch de excepciones
+            System.err.println("Ocurrio un error: " + e.getMessage());
+        }
+
         //Clientes
         Cliente cli = new Cliente(12345, "Pepe");
         Cliente cli2 = new Cliente(12346, "Pepito2");
